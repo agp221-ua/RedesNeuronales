05ad4655e4be707b0fb8dee48a0b1b740e32c2d5
agp221-ua <agp221@alu.ua.es>
2022-12-23 16:57:35 +0100

    gfhhdfgt

--- p2mnist.py
+++ p2mnist.py
@@ -73,16 +73,16 @@ def main():
     x_train = x_train.astype("float32") / 255
     x_test = x_test.astype("float32") / 255
     # Make sure images have shape (28, 28, 1)
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
+    # x_train = np.expand_dims(x_train, -1)
+    # x_test = np.expand_dims(x_test, -1)
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
-    ent = prepareData(x_train, True, 60000)
+    ent = np.array([a.flatten() for a in x_train])
     sal = prepareData(y_train, False, 60000)
 
-    enttest = prepareData(x_test, True, 10000)
+    enttest = np.array([a.flatten() for a in x_test])
     saltest = prepareData(y_test, False, 10000)
 
 
